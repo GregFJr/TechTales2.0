81e568a277db3f4e9e25b9039ced37fa3d6e37e4
GregFJr <finney101690@gmail.com>
2023-11-23 21:15:10 -0700

    another patchg for heroku deployment

--- routes/dashBoardRoutes.js
+++ routes/dashBoardRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const withAuth = require('../utils/auth');
-const dashBoardController = require('../controllers/dashBoardController');
+const dashBoardController = require('../controllers/dashboardController');
 
 
 router.get('/', withAuth, dashBoardController.renderDashboard);
